03e86b1e11f3bab0ca5ea5997caaa5754ba172ca
mitramir55 <mitra.mirshafiee@yahoo.com>
2022-01-15 12:29:32 +0330

    base template

---
+++ flask_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, flash, redirect, url_for, render_template, request
+import pandas as pd
+import os, io, csv
+from werkzeug.utils import secure_filename
+
+
+
+
+# Flask ----------------------------------------------------------------
+app = Flask(__name__)
+
+
+@app.route('/character_net', methods=['POST', 'GET'])
+def character(mode='', **kwargs):
+
+    return render_template('character_net.html')
+
+
+
+
+
+
+
+# config------------------------------------------------
+UPLOAD_FOLDER = 'uploaded_files'
+ALLOWED_EXTENSIONS = {'csv', 'xlsx'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#------------------------------------------------------
+
+
