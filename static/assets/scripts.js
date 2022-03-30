bb0f573309329fdb0ae4d1523018380a34e5db63
mitramir55 <mitra.mirshafiee@yahoo.com>
2022-03-30 18:15:14 +0430

    added side bar and icons

--- static/assets/scripts.js
+++ static/assets/scripts.js
@@ -1,10 +1,11 @@
-/*!
-* Start Bootstrap - One Page Wonder v6.0.4 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+// for the sidebar
+$(function() {
+  // Sidebar toggle behavior
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar, #content').toggleClass('active');
+  });
+});
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
